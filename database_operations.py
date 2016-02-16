1085d2ecd4e945f49d29b45791c4136b54f4c9f6
Sandor Dargo <sandor.dargo@amadeus.com>
2016-02-16 08:44:01 +0100

    Initial commit

---
+++ database_operations.py
@@ -0,0 +1,50 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
+def init_connection():
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base.metadata.bind=engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
+
+
+
+def list_all_restaurants_mock():
+    session = init_connection()
+    list_of_restaurants = list()
+    list_of_restaurants.append('Costes')
+    list_of_restaurants.append('Borkonyha')
+    return list_of_restaurants
+
+def list_all_restaurants():
+    session = init_connection()
+    query_results = session.query(Restaurant).all()
+    return query_results
+    list_of_restaurants = list()
+    for result in query_results:
+        list_of_restaurants.append(result.name)
+    return list_of_restaurants
+
+def insert_restaurant(resraurant_name):
+    session = init_connection()
+    new_restaurant = Restaurant(name=resraurant_name)
+    session.add(new_restaurant)
+    session.commit()
+
+
+def edit_restaurant(restaurant_id, restaurant_name):
+    session = init_connection()
+    restaurant_to_update = session.query(Restaurant).filter(Restaurant.id == restaurant_id).one()
+    restaurant_to_update.name = restaurant_name
+    session.add(restaurant_to_update)
+    session.commit()
+
+
+def delete_restaurant(restaurant_id):
+    session = init_connection()
+    restaurant_to_delete = session.query(Restaurant).filter(Restaurant.id == restaurant_id).one()
+    session.delete(restaurant_to_delete)
+    session.commit()
---
+++ webserver.py
@@ -0,0 +1,165 @@
+from __future__ import print_function
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+import database_operations
+from urlparse import parse_qs
+import re
+
+class WebServerHandler(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        edit_pattern = re.compile(r'/restaurants/(?P<id>(?:\d+))/edit')
+        edit_match = edit_pattern.search(self.path)
+
+        delete_pattern = re.compile(r'/restaurants/(?P<id>(?:\d+))/delete')
+        delete_match = delete_pattern.search(self.path)
+
+        if self.path.endswith("/hello"):
+            self.set_successful_response(200)
+            message = ""
+            message += "<html><body>"
+            message += "<h1>Hello!</h1>"
+            message += '''<form method='POST' enctype='multipart/form-data' action='/hello'>
+                <h2>What would you like me to say?</h2><input name="message" type="text" >
+                <input type="submit" value="Submit"> </form>'''
+            message += "</body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+
+        elif self.path.endswith("/hola"):
+            self.set_successful_response(200)
+            message = ""
+            message += "<html><body> &#161 Hola ! <a href='/hello'>Back to hello</a></body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+
+        elif self.path.endswith('/restaurants'):
+            self.set_successful_response(200)
+            message = self.get_all_restos()
+            message += "</br></br><a href='/restaurants/new'>Create a new restaurant</a>"
+            message += "</body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+
+        elif edit_match:
+            self.set_successful_response(200)
+            message = ""
+            message += "<html><body>"
+            message += "<h1>Edit restaurant!</h1>"
+            message += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/{}/edit'>
+                <h2>Please rename restaurant</h2><input name="new_restaurant_name" type="text" placeholder="{}" >
+                <input type="submit" value="Submit"> </form>'''.format(edit_match.group(1), "blabla")
+            message += "</body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+
+        elif delete_match:
+            self.set_successful_response(200)
+            message = ""
+            message += "<html><body>"
+            message += "<h1>Do you want to delete the restaurant?</h1>"
+            message += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/{}/delete'>
+                       <input type="submit" value="Submit"> </form>'''.format(delete_match.group(1))
+            message += "</body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+
+        elif self.path.endswith('/restaurants/new'):
+            self.set_successful_response(200)
+            message = ""
+            message += "<html><body>"
+            message += "<h1>Add new restaurant!</h1>"
+            message += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>
+                <h2>What is the name of the new restaurant?</h2><input name="new_restaurant_name" type="text" >
+                <input type="submit" value="Submit"> </form>'''
+            message += "</body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+        else:
+            self.send_error(404, 'File Not Found: {}'.format(self.path))
+
+    def do_POST(self):
+        try:
+
+            edit_pattern = re.compile(r'/restaurants/(?P<id>(?:\d+))/edit')
+            edit_match = edit_pattern.search(self.path)
+
+            delete_pattern = re.compile(r'/restaurants/(?P<id>(?:\d+))/delete')
+            delete_match = delete_pattern.search(self.path)
+
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            output = ""
+            if self.path.endswith('/restaurants'):
+                print('resto')
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                if self.path.endswith('/restaurants/new'):
+                    self.set_successful_response(301, '/restaurants')
+                    print('resto')
+                    messagecontent = fields.get('new_restaurant_name')
+                    print(messagecontent)
+                    database_operations.insert_restaurant(str(messagecontent[0]))
+                elif self.path.endswith('/hello'):
+                    self.set_successful_response(301)
+                    messagecontent = fields.get('message')
+                    output += "<html><body>"
+                    output += " <h2> Okay, how about this: </h2>"
+                    output += "<h1> {} </h1>".format(messagecontent[0])
+                    output += '''<form method='POST' enctype='multipart/form-data' action='/hello'>
+                        <h2>What would you like me to say?</h2>
+                        <input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                    output += "</body></html>"
+                elif edit_match:
+                    self.set_successful_response(301, '/restaurants')
+                    print('resto changed')
+                    messagecontent = fields.get('new_restaurant_name')
+                    print(messagecontent)
+                    database_operations.edit_restaurant(edit_match.group(1), str(messagecontent[0]))
+                elif delete_match:
+                    self.set_successful_response(301, '/restaurants')
+                    print('resto deleted')
+                    database_operations.delete_restaurant(delete_match.group(1))
+
+
+            self.wfile.write(output)
+            print(output)
+        except:
+            pass
+
+
+    def set_successful_response(self, status_id, location=None):
+        self.send_response(status_id)
+        self.send_header('Content-type', 'text/html')
+        if location:
+            self.send_header('Location', location)
+        self.end_headers()
+
+    def get_all_restos(self):
+        message = ''
+        message += '<html><body> List of all restaurants: </body></html>'
+        for restaurant in database_operations.list_all_restaurants():
+            message += '''<p>{} <a href='restaurants/{}/edit'>Edit</a> <a href='restaurants/{}/delete'>Delete</a></p>'''.format(restaurant.name, restaurant.id, restaurant.id)
+        return message
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), WebServerHandler)
+        print('Web server is running on port {}'.format(port))
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("^C entered, stopping web server...")
+        server.socket.close()
+        raise
+
+
+if __name__ == '__main__':
+    main()
